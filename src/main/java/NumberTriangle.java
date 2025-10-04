b84b0de04303af5a75a0d9645b3a996a5c2b72aa
MSTNDL <140838491+MSTNDL@users.noreply.github.com>
2025-10-03 20:52:04 -0400

    completed loadTriangle method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -90,7 +90,6 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
         return -1;
     }
 
@@ -111,32 +110,41 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
         List<List<NumberTriangle>> rows = new ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
 
-        String line = br.readLine();
-        NumberTriangle top = new NumberTriangle(Integer.parseInt(line));
-        while (line != null) {
+        String line;
+        NumberTriangle top = null;
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+        while ((line = br.readLine()) != null) {
 
-
-            // TODO process the line
-            List<NumberTriangle> currRow = new ArrayList<>();
             String[] currLine = line.split(" ");
+            List<NumberTriangle> currRow = new ArrayList<>();
+
             for (String node : currLine) {
                 currRow.add(new NumberTriangle(Integer.parseInt(node)));
             }
-            rows.add(currRow);
 
-            //read the next line
-            line = br.readLine();
+            if (!rows.isEmpty()) {
+                List<NumberTriangle> prevRow = rows.get(rows.size() - 1);
+                for (int i = 0; i < currRow.size(); i++) {
+                    if (i == 0) {
+                        prevRow.get(i).setLeft(currRow.get(i));
+                    } else if (i == currRow.size() - 1) {
+                        prevRow.get(i - 1).setRight(currRow.get(i));
+                    } else {
+                        prevRow.get(i - 1).setRight(currRow.get(i));
+                        prevRow.get(i).setLeft(currRow.get(i));
+                    }
+                }
+            } else {
+                top = currRow.get(0);
+            }
+            rows.add(currRow);
         }
+
         br.close();
         return top;
     }
